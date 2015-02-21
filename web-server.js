0edcc3ecf1c01f655652b0bb80a49053f7f1dd26
V.J. Clasgens <vj@parkifi.com>
2015-02-20 22:36:03 -0700

    Cleaned up webserver updated circle.yml file

--- web-server.js
+++ web-server.js
@@ -1,32 +1,3 @@
-//var express = require('express');
-//var path = require('path');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
-//
-//var app = express();
-//
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'app')));
-//
-//
-//app.set('port', process.env.PORT || 7000);
-//
-//app.listen(app.get('port'), function() {
-//    console.log('Express server listening on port ' + app.get('port'));
-//});
-//
-//// express.static() first...
-//app.use(express.static(__dirname+'/app'));
-//// ...followed by your catch-all route (only one needed)
-//app.get('*', function(req, res) {
-//    res.render('app/index.html');
-//});
-//app.get('/*', function(req, res) {
-//    res.render('app/index.html');
-//});
-
 var express = require('express');
 //var path = require('path');
 var app = express();
@@ -41,11 +12,6 @@ app.use('/', express.static(__dirname + '/app'));
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
-
-// Make sure the app redirects to the same place
-//app.get('/*', function(req, res) {
-//    res.sendfile(path.join(__dirname, 'app', 'index.html'));
-//});
 
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
